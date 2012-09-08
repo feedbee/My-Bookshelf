de3e716976e0edc86a6b196d2a08adc0183db6ac
feedbee <feedbee@feedbee.home>
2012-09-08 22:06:59 +0300

    Bookshelf

---
+++ feedbee.php
@@ -0,0 +1,53 @@
+<?php
+	$xmlstr = file_get_contents('data/feedbee.xml');
+	$bookshelf = new SimpleXMLElement($xmlstr);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>feedbee — My Bookshelf</title>
+	<link rel="stylesheet" href="styles.css" type="text/css" />
+	<link rel="stylesheet" href="bookshelf.css" type="text/css" />
+</head>
+
+<body>
+	<div id="page-wrapper">
+		<div id="content-wrapper">
+			<header>
+				<div>&lt;<strong>My Bookshelf</strong>&gt;</div>
+			</header>
+			<h1>feedbee's bookshelf</h1>
+			
+			<?php if (count($bookshelf->book) < 1) { ?>
+			<p><small>Bookshelf is empty.</small></p>
+			<?php } else { ?>
+			<ul class="bookshelf">
+				<?php foreach ($bookshelf->book as $book) { ?>
+				<li class="bookshelf-book">
+					<img src="<?php echo $book->cover; ?>" alt="Обложка" class="bookshelf-book-cover" />
+					<div class="bookshelf-book-info">
+						<h3 class="bookshelf-book-info-title bookshelf-name"><a href="<?php echo $book->url; ?>"><?php echo $book->name; ?></a></h3>
+						<p class="bookshelf-book-info-authors">
+							<?php $authorsCount = count($book->authors->author); $i = 0;
+								foreach ($book->authors->author as $author) { ?>
+								<a href="<?php echo $author->url; ?>"><?php echo $author->name; ?></a><?php if (++$i < $authorsCount) { ?>, <?php } ?>
+							<?php } ?>
+						</p>
+						<p class="bookshelf-book-info-publication">
+							Издательство <span class="bookshelf-name"><a href="<?php echo $book->publish->publisher->url; ?>"><?php echo $book->publish->publisher->name; ?></a></span>; <span class="bookshelf-pages"><?php echo $book->publish->pages; ?></span>; <span class="bookshelf-year"><?php echo $book->publish->year; ?></span>
+						</p>
+						<div class="bookshelf-raiting-outer">
+							<div class="bookshelf-raiting-inner" style="width:<?php echo round((string)$book->my->rating) * 20; ?>%"></div>
+						</div>
+						<p class="bookshelf-review"><?php echo $book->my->review; ?></p>
+					</div>
+				</li>
+				<?php } ?>
+			</ul>
+			<?php } ?>
+		</div>
+		<footer>&copy; Valera Leontyev, 2012.</footer>
+	</div>
+</body>
+</html>
